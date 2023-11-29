ffccbb581d0f26e6fc9c68ecf9299301f7395864
silatcha <silatchayvan@gmail.com>
2023-11-29 18:37:16 +0100

    first commit

--- broker/main.py
+++ broker/main.py
@@ -1,4 +1,4 @@
 from kafka.producer import KafkaProducer
-producer = KafkaProducer(bootstrap_servers='localhost:9096')
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
 for _ in range(10):
   producer.send('foobar',b'dddd')
