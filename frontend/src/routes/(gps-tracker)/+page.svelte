83c6a0f26e816c77c21ef3af80fbc741a235381c
MoonLyss <shaitan.lyssm@gmail.com>
2023-12-17 18:08:01 +0100

    Add houdini

--- frontend/src/routes/(gps-tracker)/+page.svelte
+++ frontend/src/routes/(gps-tracker)/+page.svelte
@@ -1,11 +1,26 @@
-    <script lang="ts">
-        import {MapLibre} from 'svelte-maplibre'
+<script lang="ts">
+	import { MapLibre, Marker, type Map } from 'svelte-maplibre';
+	import Fa from 'svelte-fa';
+	import {faKiwiBird, type IconDefinition} from '@fortawesome/free-solid-svg-icons'
+	import {faTwitter} from '@fortawesome/free-brands-svg-icons'
+	import { onDestroy, onMount } from 'svelte';
 
-        
-    </script>
+	const icons: IconDefinition[] = [faKiwiBird, faTwitter]
 
-    <MapLibre
-  style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
-  class="relative w-full aspect-[9/16] max-h-[70vh] sm:max-h-full sm:aspect-video"
-  standardControls
-/>
+	let map: Map | undefined;
+	onMount(() => {
+		console.log(map);
+		
+	});
+</script>
+
+<MapLibre
+	style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+	class="relative w-full aspect-[9/16] max-h-[70vh] sm:max-h-full sm:aspect-video"
+	standardControls
+	bind:map={map}
+>
+	<Marker lngLat={[0, 0]}>
+		<Fa icon={faKiwiBird}/>
+	</Marker>
+</MapLibre>
