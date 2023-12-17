83fec49a848e2ad30ab69a729c3ba3237da67d3c
MoonLyss <shaitan.lyssm@gmail.com>
2023-12-17 17:00:48 +0100

    Ok back to starters

---
+++ frontend/src/routes/(gps-tracker)/+page.svelte
@@ -0,0 +1,11 @@
+    <script lang="ts">
+        import {MapLibre} from 'svelte-maplibre'
+
+        
+    </script>
+
+    <MapLibre
+  style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+  class="relative w-full aspect-[9/16] max-h-[70vh] sm:max-h-full sm:aspect-video"
+  standardControls
+/>
