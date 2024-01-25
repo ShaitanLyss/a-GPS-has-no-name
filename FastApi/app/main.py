cdc317b0b7cfa1a79fbad50635bcc4545c8194a5
silatcha <silatchayvan@gmail.com>
2024-01-25 12:04:26 +0100

    merge all branch

--- FastApi/app/main.py
+++ FastApi/app/main.py
@@ -1,6 +1,14 @@
 import os
 from typing import List
+import asyncio
+from math import sin
+import math
+import random
+from typing import AsyncGenerator, List
+import strawberry
 
+from fastapi.middleware.cors import CORSMiddleware
+from strawberry.fastapi import GraphQLRouter
 from fastapi.params import Depends, Query
 from shapely import wkb
 from shapely.geometry import Point
@@ -41,7 +49,111 @@ class LocationCreate(BaseModel):
     long: float
 
 
+
+
+
+@strawberry.type
+class Location:
+    lat: float
+    lng: float
+
+
+@strawberry.type
+class TrackedObject:
+    ip: str
+    location: Location
+
+
+def random_number(a: float, b = None):
+    if b is None:
+        b = a
+        a = -a
+    return a + random.random() * (b - a)
+
+
+def random_location():
+    return Location(lat=random_number(90), lng=random_number(180))
+
+
+tracked_objects = [
+    TrackedObject(ip="ye", location=random_location()),
+    TrackedObject(ip="ye2", location=random_location()),
+]
+
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def trackedObjects(self) -> list[TrackedObject]:
+        return tracked_objects
+
+
+@strawberry.type
+class Subscription:
+    @strawberry.subscription
+    async def count(self, target: int = 100) -> AsyncGenerator[int, None]:
+        for i in range(target):
+            yield i
+            await asyncio.sleep(0.001)
+
+
+    @strawberry.subscription
+    async def objectsUpdated(self) -> AsyncGenerator[List[TrackedObject], None]:
+        dt = 1e-3
+        while True:
+            yield tracked_objects
+            await asyncio.sleep(dt)
+            for i in range(len(tracked_objects)):
+                T_lng = 5 if i % 2 else 1
+                w_lng = 360 / T_lng
+                T_lat = 1
+                w_lat = 180 / T_lat
+
+                lat = tracked_objects[i].location.lat
+                lng = tracked_objects[i].location.lng
+
+                lat += 90
+                lng += 180
+
+                lat = ( lat + w_lat * dt) % 180
+                lng = (lng + (-1)**i * w_lng * dt) % 360
+
+                lat -= 90
+                lng -= 180
+
+                tracked_objects[i].location.lat = lat
+                tracked_objects[i].location.lng = lng
+
+
+
+
+schema = strawberry.Schema(query=Query, subscription=Subscription)
+
+graphql_app = GraphQLRouter(schema)
+
 app = FastAPI()
+
+
+
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost:5173",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://localhost:4000",
+    "http://fastapi:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+#app.include_router(graphql_app, prefix="/graphql")
 
 
 # Dependency to get the database session
@@ -61,6 +173,13 @@ async def create_location(location: LocationCreate):
     db.refresh(db_location)
     db.close()
     return db_location
+
+@app.get("/")
+async def create_location():
+    db = SessionLocal()
+    location = db.query(Location).all()
+    db.close()
+    return location
 
 
 @app.get("/c/{lib}", response_model=LocationCreate)
@@ -101,3 +220,10 @@ async def read_locations(
         location_dict['long'] = list(geometry.coords[0])[1]
         results.append(location_dict)
     return results
+
+
+
+
+
+
+
